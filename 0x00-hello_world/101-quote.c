007c0b2013e8e847833b70886c1eed5bb2c6d327
Saysherief <suhairdama@gmail.com>
2023-02-16 19:43:51 -0800

    C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,12 @@
 #include <unistd.h>
-
 /**
  * main - prints to string
  * Description: Prints without puts and printf
- * Returns 1
+ * Returns: 1 (Failure)
  */
-
 int main(void)
 {
-	char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, msg, 59);
-	return (1);
+char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, msg, 59);
+return (1);
 }
