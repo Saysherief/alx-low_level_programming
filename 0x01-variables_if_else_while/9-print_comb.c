2273ac030149178d32c105845a0200cd316af739
Saysherief <suhairdama@gmail.com>
2023-02-18 03:18:57 -0800

    prints all possible combinations of single-digit numbers separated by , and space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: prints the alphabet in lowercase using putchar, then new line
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int dig;
+
+	for (dig = 0; dig < 10; dig++)
+	{
+		putchar(dig + '0');
+		if (dig == 9)
+			continue;
+		putchar(' ');
+		putchar(',');
+	}
+	putchar('$');
+	return (0);
+}
