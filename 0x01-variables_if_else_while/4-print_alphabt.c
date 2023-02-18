98e2471c180db93c2a0ac9d45f925d0d89268b77
Saysherief <suhairdama@gmail.com>
2023-02-18 01:30:01 -0800

    Print all the letters except q and e using putchar

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: prints the alphabet in lowercase using putchar, then new line
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		if (alpha == 'e' || alpha == 'q')
+			continue;
+		putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+}
