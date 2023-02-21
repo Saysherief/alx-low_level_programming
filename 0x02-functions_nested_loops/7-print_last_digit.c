ce1463508b89fbb8193bd730a46a776bb6c01ab6
Saysherief <suhairdama@gmail.com>
2023-02-21 14:27:55 -0800

    computes the last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * Description - computes using modulo
+ * @n: an intiger
+ * Return: the last digit of a number
+ */
+int _abs(int n)
+{
+	n = n % 10;
+	return (n);
+}
