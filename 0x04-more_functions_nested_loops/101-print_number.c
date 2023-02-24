2698ad3434ef3d237260b74a011be375397f04f3
Saysherief <suhairdama@gmail.com>
2023-02-24 15:37:10 -0800

    Prints the exact intiger using _putchar()

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_number - prints the number taken in the argument
+ * Description: prints using _putchar, then new line
+ * @n: input integer
+ * Return: Nothing
+ */
+void print_number(int n)
+{
+	unsigned int dig = 1, value = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		value *= -1;
+		n *= -1;
+	}
+	while (n > 9)
+	{
+		n /= 10;
+		dig *= 10;
+	}
+	for (; dig >= 1; dig /= 10)
+	{
+		_putchar(((value / dig) % 10) + '0');
+	}
+	_putchar('\n');
+}
