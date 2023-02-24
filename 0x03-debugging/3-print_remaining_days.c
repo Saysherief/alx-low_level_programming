5ca07670b5c8162a3add931a3515b271bee3ec94
Saysherief <suhairdama@gmail.com>
2023-02-24 02:22:55 -0800

    Fixed the print_remaining_days() function

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,6 +12,7 @@
 
 void print_remaining_days(int month, int day, int year)
 {
+	int convert_day(int month, int day);
 	if ((year % 4 != 0 && year % 400 != 0) || (year % 100 == 0))
 	{
 		if (month >= 2 && day >= 60)
