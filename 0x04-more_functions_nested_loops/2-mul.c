e7cc71d9256c87994988012b9c26ff8958832be1
Saysherief <suhairdama@gmail.com>
2023-02-24 12:12:28 -0800

    multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _mul - multiplies two integers
+ * Description - uses two integer parameters @a and @b
+ *
+ * Return: integer multiplication of @a and @b
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
