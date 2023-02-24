dd9bec1b6ababe7dbcb0a5b6906d50050e8f2f26
Saysherief <suhairdama@gmail.com>
2023-02-24 13:14:57 -0800

    Draws a line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,7 +2,7 @@
 
 /**
  * print_line - draws a straight line in the terminal
- * @n - number of times _ shoulld be printed
+ * @n : number of times _ shoulld be printed
  * line should end with new line
  * Return : Nothing
  */
