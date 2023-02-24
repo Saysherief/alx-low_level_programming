134c5c93c6e0d89c841f53f80c0384cb4d6f7d40
Saysherief <suhairdama@gmail.com>
2023-02-24 14:20:02 -0800

    The Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+/**
+ * main - Starting point of a function
+ * Description: prints Fizz-Buzz-FizzBuzz whenever needed
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	num = 1;
+	printf("%d", num);
+	for (num = 2; num <= 100; num++)
+	{
+		if (((num % 3) == 0) && ((num % 5) == 0))
+		{
+			printf(" FizzBuzz");
+		}
+		else if ((num % 3) == 0)
+		{
+			ptintf(" Fizz");
+		}
+		else if ((num % 5) == 0)
+		{
+			printf(" Buzz");
+		}
+		else
+		{
+			printf(" %d", num);
+		}
+	}
+	printf("\n");
+	return (0);
+}
