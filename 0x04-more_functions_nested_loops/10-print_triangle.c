ba89cfc0d4ef31200ba72560e35dd83581efcc62
Saysherief <suhairdama@gmail.com>
2023-02-24 14:43:16 -0800

    Draw  a triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -15,7 +15,7 @@ void print_triangle(int size)
 		for (j = 1; j < (size - i); j++)
 			_putchar(' ');
 		for (j--; j < size; j++)
-			_putchar('\#');
+			_putchar('#');
 		if (i < (size - 1))
 			_putchar('\n');
 	}
