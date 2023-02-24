3e40d3b24f34cd19f78ea08a2b19655ade1af88b
Saysherief <suhairdama@gmail.com>
2023-02-24 11:30:46 -0800

    checks for uppercase character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _islower - retutns 1 for uppercase letters and 0 for the rest
+ * Description - uses the parameter @c to test the condition
+ * @c: The character to check the ASCII code
+ * Return: int 0 or 1
+ */
+
+int _isupper(int c)
+{
+	return (isupper(c));
+}
