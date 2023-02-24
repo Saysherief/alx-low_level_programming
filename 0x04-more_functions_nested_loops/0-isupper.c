e368b7c67059871cd9662e0466eaa29fdb89412f
Saysherief <suhairdama@gmail.com>
2023-02-24 11:47:32 -0800

    checks for uppercase character

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,12 +4,12 @@
  * _isupper - retutns 1 for uppercase letters and 0 for the rest
  * Description - uses the parameter @c to test the condition
  * @c: The character to check the ASCII code
- * Return: int 0 or 1
+ * Return: 1 if it is an uppercase character else 0
  */
 
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 91)
+	if (c >= 65 && c <= 90)
 	{
 		return (1);
 	}
