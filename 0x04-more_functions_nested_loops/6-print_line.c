5ecfdfc0c5344f7c275a9816fb5622c59998f656
Saysherief <suhairdama@gmail.com>
2023-02-24 13:10:37 -0800

    Draws a line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ * @n - number of times _ shoulld be printed
+ * line should end with new line
+ * Return : Nothing
+ */
+void print_line(int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+	{
+		if (n <= 0)
+			break;
+		else
+			_putchar('_');
+	}
+	_putchar('\n');
+}
