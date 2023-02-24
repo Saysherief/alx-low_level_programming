5d61e316747c18b4ec8a3fc481743a2a3b5ae6ed
Saysherief <suhairdama@gmail.com>
2023-02-24 14:38:04 -0800

    Draw  a triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_triangle - draws a triangle in the terminal
+ * @size : number of times # should be printed
+ * line should end with new line
+ * Return : Nothing
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 1; j < (size - i); j++)
+			_putchar(' ');
+		for (j--; j < size; j++)
+			_putchar('\#');
+		if (i < (size - 1))
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
