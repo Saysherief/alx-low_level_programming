c7cb19698c06f8808dc0fb3d4c894ea7d8e9bef9
Saysherief <suhairdama@gmail.com>
2023-02-27 15:18:05 -0800

    prints the second half of the string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,8 +13,10 @@ void puts_half(char *str)
 	{
 		len++;
 	}
-	len--;
-	len = len / 2;
+	if ((len % 2) == 0)
+		len = len / 2;
+	else
+		len = ++len/2;
 	while (str[len] != '\0')
 	{
 		_putchar(str[len]);
