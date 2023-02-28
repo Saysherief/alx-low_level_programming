3b14505159fba5d49fb6fcaabb41097b02201684
Saysherief <suhairdama@gmail.com>
2023-02-27 16:45:21 -0800

    copies a string into another

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies the string pointed by src
+ * @src: a pointer to the string /the first char on the string
+ * @dest: a pointer to copy at a destination
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len = 0;
+
+	while (src[len] != '\0')
+	{
+		dest[len] = src[len];
+		len++;
+	}
+	dest[len] = '\0';
+	return (*dest);
+}
