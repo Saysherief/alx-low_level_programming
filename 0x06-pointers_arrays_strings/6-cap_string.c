bfd2b3a3c978bdb2070ef674ad763f3aad1513f5
Saysherief <suhairdama@gmail.com>
2023-03-02 16:17:51 -0800

    capitalizes all words of a string after separators

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string after separators
+ * @s: a pointer to the string
+ * Return: address of the changed string
+ */
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i - 1] == ' ' || s[i - 1] == '\t' || s[i - 1] == '\n'
+				|| s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.'
+				|| s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"'
+				|| s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{'
+				|| s[i - 1] ==  '}')
+			if (s[i] >= 'a' && s[i] <= 'z')
+				s[i] -= 32;
+		i++;
+	}
+	return (s);
+}
