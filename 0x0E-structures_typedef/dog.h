0e91c7b131dcaf26c4496431061298fd07ede6c4
Saysherief <suhairdama@gmail.com>
2023-03-21 19:42:31 -0700

    poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog's description
+ * @name: First member string
+ * @age: Second member float
+ * @owner: Third member string
+ *
+ * Description: These will be the character of any dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
