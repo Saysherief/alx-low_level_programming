9049e27fef393575c3955f9899fe3922049b0a3d
Saysherief <suhairdama@gmail.com>
2023-03-23 12:23:05 -0700

    If separator is NULL, don’t print it

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,18 +13,15 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list nums;
 	unsigned int i;
 
-	if (separator != NULL)
-	{
-		va_start(nums, n);
+	va_start(nums, n);
 
-		for (i = 0; i < n; i++)
-		{
-			printf("%d", va_arg(nums, unsigned int));
-			if (i == (n - 1))
-				break;
-			printf("%s", separator);
-		}
-		va_end(nums);
-		printf("\n");
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(nums, unsigned int));
+		if (i == (n - 1) || separator == NULL)
+			break;
+		printf("%s", separator);
 	}
+	va_end(nums);
+	printf("\n");
 }
