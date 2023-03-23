b47b562f11560ce2355a42f0ec9207a2620f2aa8
Saysherief <suhairdama@gmail.com>
2023-03-22 17:05:47 -0700

    mini operator?

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -16,8 +16,13 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		exit(98);
 	}
+	if (argv[2][1])
+	{
+		printf("Error\n");
+		exit(99);
+	}
 	op = get_op_func(argv[2]);
-	if (argv[2][1] || (op == NULL))
+	if (op == NULL)
 	{
 		printf("Error\n");
 		exit(99);
