9e8e55dae8a4ec76c585443297e2a55ce277e040
Saysherief <suhairdama@gmail.com>
2023-03-23 12:30:23 -0700

    Just reversed the logic

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,10 +18,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(nums, unsigned int));
-		if (i == (n - 1) || separator == NULL)
-			break;
-		printf("%s", separator);
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
 	}
-	va_end(nums);
 	printf("\n");
+	va_end(nums);
 }
