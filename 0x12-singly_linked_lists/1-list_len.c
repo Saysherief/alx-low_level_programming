940a09e352fdee708e65aa7f3eacc0dbc2c38eed
Saysherief <suhairdama@gmail.com>
2023-03-30 19:16:19 -0700

    Prints lists of elements in the node and returns no. of elements

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,20 +1,16 @@
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a list_t list
+ * list_len - returns the number of elements in a linked list_t list
  * @h: pointer to the first node of list_t
  * Return: the number of nodes
  */
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
 	size_t nnodes = 0;
 
 	while (h != NULL)
 	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
-		else
-			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		nnodes++;
 	}
