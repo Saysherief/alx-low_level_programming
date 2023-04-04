ba2ff34c29f26f215663bab71ba7adc12631861c
Saysherief <suhairdama@gmail.com>
2023-04-04 09:57:20 -0700

    Checked all possibilities

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,5 +1,7 @@
 #include "lists.h"
 
+listint_t *insertnode(listint_t **head, int n);
+size_t listint_len(const listint_t *h);
 /**
  * insert_nodeint_at_index - inserts a new node at a given position
  * @head: the pointer to the pointer of the first node of listint_t
@@ -9,15 +11,14 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *tmp, *tmp2, *nth = NULL;
-	unsigned int i = 0;
+	listint_t *tmp = *head, *tmp2, *nth = NULL;
+	unsigned int i = 0, len = (listint_len(tmp));
 
-	if ((*head == NULL) && (idx == 0))
+	if (idx > len)
+		return (NULL);
+	else if ((*head == NULL) && (idx == 0))
 	{
-		nth = malloc(sizeof(listint_t));
-		if (nth == NULL)
-			return (NULL);
-		nth->n = n;
+		nth = insertnode(&tmp, n);
 		nth->next = *head;
 		*head = nth;
 	}
@@ -25,17 +26,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		if (idx == 0)
 		{
-			nth = malloc(sizeof(listint_t));
-			if (nth == NULL)
-				return (NULL);
-			nth->n = n;
+			nth = insertnode(&tmp, n);
 			nth->next = *head;
 			*head = nth;
 		}
 		else
 		{
 			nth = *head;
-			while (i < idx - 1)
+			while ((i < idx - 1) && nth != NULL)
 			{
 				nth = nth->next;
 				i++;
@@ -51,4 +49,40 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 	}
 	return (nth);
+}
+
+/**
+ * insertnode - insets a node at the very begining
+ * @head: pointer to the first node
+ * @n: value of the node
+ * Return pointer to the inserted node
+ */
+listint_t *insertnode(listint_t **head, int n)
+{
+	listint_t *nth;
+
+	nth = malloc(sizeof(listint_t));
+	if (nth == NULL)
+		return (NULL);
+	nth->n = n;
+	nth->next = *head;
+	*head = nth;
+	return (nth);
+}
+
+/**
+ * listint_len - returns the number of elements in a linked listint_t list
+ * @h: pointer to the first node of listint_t
+ * Return: the number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t nnodes = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		nnodes++;
+	}
+	return (nnodes);
 }
