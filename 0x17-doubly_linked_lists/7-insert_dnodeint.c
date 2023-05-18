2bfe3945b21083934e6a3f28e0330b06eab238b8
Saysherief <suhairdama@gmail.com>
2023-05-18 14:28:11 -0700

    inserts node at an index in a dlistint_t list

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,76 @@
+#include "lists.h"
+
+size_t dlistint_len(const dlistint_t *h);
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * @head: the pointer to the pointer of the first node of dlistint_t
+ * @idx: the index of the new node to be added in dlistint_t
+ * @n: value of the new node
+ * Return: the address of the nth node or NULL if it fails
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int idx, int n)
+{
+	dlistint_t *tmp = *head, *tmp2, *nth = NULL;
+	unsigned int i = 0, len = (dlistint_len(tmp));
+
+	if (idx > len)
+		return (NULL);
+	else if ((*head == NULL) && (idx == 0))
+	{
+		nth = add_dnodeint(&tmp, n);
+		nth->next = NULL;
+		nth->prev = NULL;
+		*head = nth;
+	}
+	else
+	{
+		if (idx == 0)
+		{
+			nth = add_dnodeint(&tmp, n);
+			nth->next = *head;
+			nth->prev = NULL;
+			*head = nth;
+		}
+		else if (idx == len)
+		{
+			nth = add_dnodeint_end(&tmp, n);
+		}
+		else
+		{
+			nth = *head;
+			while ((i < idx - 1) && nth != NULL)
+			{
+				nth = nth->next;
+				i++;
+			}
+			tmp = nth;
+			tmp2 = nth->next;
+			nth = malloc(sizeof(dlistint_t));
+			if (nth == NULL)
+				return (NULL);
+			nth->n = n;
+			tmp->next = nth;
+			nth->prev = tmp;
+			nth->next = tmp2;
+			tmp2->prev = nth;
+		}
+	}
+	return (nth);
+}
+
+/**
+ * dlistint_len - returns the number of elements in a dlistint_t list
+ * @h: pointer to the first node of dlistint_t
+ * Return: the number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t nnodes = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		nnodes++;
+	}
+	return (nnodes);
+}
